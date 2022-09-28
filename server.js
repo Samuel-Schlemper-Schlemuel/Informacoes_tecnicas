5007a25d1e5445306de6ce5240c4ee5c4236bfc0
Samuel Schlemper <samuelschlemper2006@gmail.com>
2022-09-27 22:02:52 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const os = require('os')
+const app = express()
+app.use(express.static('.'));
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/HTML.html')
+})
+
+app.get('/api/whoami', (req, res)=>{
+    const networkInfo = os.networkInterfaces();
+    const api = networkInfo['Wi-Fi'][1].address
+    const language =  req.headers["accept-language"]
+    const software = req.headers["user-agent"]
+
+    res.json({ipaddress: api, language: language, software: software})
+})
+
+app.listen('samuel-schlemper-schlemuel.github.io/Informacoes_tecnicas')
