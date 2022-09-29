9095bb2005c085a29b4ceddf1d2c7f3bb95e6ca1
Samuel Schlemper <samuelschlemper2006@gmail.com>
2022-09-29 07:19:57 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const os = require('os')
 const app = express()
+console.log(os)
 const PORT = process.env.PORT || 5050
-app.use(express.static('Public'))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/HTML.html')
 })
 
 app.get('/api/whoami', (req, res)=>{
+    console.log('chegou')
     const networkInfo = os.networkInterfaces()
     const api = networkInfo['Wi-Fi'][1].address
     const language =  req.headers["accept-language"]
